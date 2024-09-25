a7c6399725162466aeaf1a4cbc2c8ce81d92bb60
350444535 <150150007+350444535@users.noreply.github.com>
2024-09-25 12:17:06 -0400

    apple actor

--- MyWorld.java
+++ MyWorld.java
@@ -15,7 +15,52 @@ public class MyWorld extends World
      */
     public MyWorld()
     {    
+        
+    public int score = 0;
+    Label scoreLabel;
+    
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(600, 400, 1, false);
+        
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 350);
+        
+        scoreLabel = new Label(0, 80);
+        addObject(scoreLabel, 50, 50);
+        
+        createApple();
+    }
+    
+        public void gameOver()
+    {   
+        /**
+         * adds a gameover label when player loses the game
+         */
+        Label gameOverLabel = new Label("Game Over", 100);
+        addObject(gameOverLabel, 300, 200);
+    }
+    
+    public void increaseScore()
+    {
+        score++;
+        scoreLabel.setValue(score);
+    }
+    
+    public void createApple()
+    {   
+        /**
+         * adds apple at random location
+         */
+        Apple apple = new Apple();
+        int x = Greenfoot.getRandomNumber(600);
+        
+        addObject(apple, x, 0);
+    } 
     }
 }
