8e9a86b5c49c613df1f9a9b146414623e15ea462
350444535 <150150007+350444535@users.noreply.github.com>
2024-09-25 12:38:30 -0400

    Update MyWorld.java

--- MyWorld.java
+++ MyWorld.java
@@ -35,7 +35,6 @@ public class MyWorld extends World
     {
         score++;
         scoreLabel.setValue(score);
-        super(600, 400, 1);
         
         Elephant elephant = new Elephant();
         addObject(elephant, 300,300);
